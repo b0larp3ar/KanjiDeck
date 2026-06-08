5d29fdc6141475848652e0ee15bcc2b283f403e2
b0larp3ar <148764066+b0larp3ar@users.noreply.github.com>
2026-06-09 04:12:45 +0530

    SRS and statistics page

--- app.py
+++ app.py
@@ -10,14 +10,38 @@ def home():
     return render_template("home.html")
 
 #review page
+a=0
 @app.route("/review/<level>")
 def review(level):
     conn=sqlite3.connect("vocabulary.db")
     cursor=conn.cursor()
-    cursor.execute("SELECT id, original, furigana, english FROM Vocabulary WHERE level=? ORDER BY RANDOM() LIMIT 1", (level,))
-    card=cursor.fetchone()
+
+    global a
+
+    
+    card=()
+
+    if a==0:
+        cursor.execute("SELECT id from Vocabulary WHERE position=? ORDER BY RANDOM() LIMIT 1", (a,))
+        elementid=cursor.fetchone()[0]
+        cursor.execute("UPDATE Vocabulary SET position=? WHERE id=?", (1, elementid))
+        a+=1
+
+    if card==():
+        cursor.execute("SELECT id, original, furigana, english, correct, incorrect, position from Vocabulary WHERE position=? ORDER BY RANDOM()", (a,))
+        card=cursor.fetchone()
+        
+        if card is None:
+            cursor.execute("SELECT id, original, furigana, english, correct, incorrect, position FROM Vocabulary WHERE position=0 ORDER BY RANDOM()")
+            card=cursor.fetchone()
+            cursor.execute("UPDATE Vocabulary SET position=? WHERE id=?", (a, card[0]))
+        a+=1
+
+    conn.commit()
     conn.close()
-    return render_template("review.html", id=card[0], original=card[1], furigana=card[2], english=card[3], level=level)
+    if card==():
+        print("No card selection")
+    return render_template("review.html", id=card[0], original=card[1], furigana=card[2], english=card[3], level=level, correct=card[4], incorrect=card[5], position=card[6])
 
 #Updates reviewHistory.db when user reviews a card
 @app.route("/correct", methods=["POST"])
@@ -25,10 +49,27 @@ def correct():
     card_id=request.form["card_id"]
     level=request.form["level"]
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    correct=int(request.form["correct"])+1
+    incorrect=int(request.form["incorrect"])
 
     conn=sqlite3.connect("reviewHistory.db")
     cursor=conn.cursor()
     cursor.execute(f"INSERT INTO ReviewHistory(card_id, level, result, timestamp) VALUES(?, ?, ?, ?)", (card_id, level, "Correct", timestamp))
+
+    conn.commit()
+    conn.close()
+
+    conn=sqlite3.connect("vocabulary.db")
+    cursor=conn.cursor()
+    cursor.execute("UPDATE Vocabulary SET correct=? WHERE id=?", (correct, card_id))
+
+    total=correct+incorrect
+    correct_percentage=(correct/total)*100
+
+    position = max(1, round(10 * (1.05 ** (correct_percentage - 50))))
+    
+    global a
+    cursor.execute("UPDATE Vocabulary SET position=? WHERE id=?", (position+a, card_id))
 
     conn.commit()
     conn.close()
@@ -40,6 +81,8 @@ def incorrect():
     card_id=request.form["card_id"]
     level=request.form["level"]
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    correct=int(request.form["correct"])
+    incorrect=int(request.form["incorrect"])+1
 
     conn=sqlite3.connect("reviewHistory.db")
     cursor=conn.cursor()
@@ -48,7 +91,64 @@ def incorrect():
     conn.commit()
     conn.close()
 
+    conn=sqlite3.connect("vocabulary.db")
+    cursor=conn.cursor()
+    cursor.execute("UPDATE Vocabulary SET incorrect=? WHERE id=?", (incorrect, card_id))
+
+    total=correct+incorrect
+    correct_percentage=(correct/total)*100
+
+    position = max(1, round(10 * (1.05 ** (correct_percentage - 50))))
+    
+    global a
+    cursor.execute("UPDATE Vocabulary SET position=? WHERE id=?", (position+a, card_id))
+    conn.commit()
+    conn.close()
+
     return redirect(f"/review/{level}")
+
+#statistics page
+@app.route("/statistics")
+def statistics():
+    conn=sqlite3.connect("reviewHistory.db")
+    cursor=conn.cursor()
+
+    #number of correct reviews
+    cursor.execute("SELECT COUNT(*) FROM ReviewHistory WHERE result='Correct'")
+    correct=cursor.fetchone()[0]
+
+    #number of incorrect reviews
+    cursor.execute("SELECT COUNT(*) FROM ReviewHistory WHERE result='Incorrect'")
+    incorrect=cursor.fetchone()[0]
+
+    #number of N5 reviews
+    cursor.execute("SELECT COUNT(*) FROM ReviewHistory WHERE level='N5'")
+    n5=cursor.fetchone()[0]
+
+    #number of N4 reviews
+    cursor.execute("SELECT COUNT(*) FROM ReviewHistory WHERE level='N4'")
+    n4=cursor.fetchone()[0]
+
+    #number of N3 reviews
+    cursor.execute("SELECT COUNT(*) FROM ReviewHistory WHERE level='N3'")
+    n3=cursor.fetchone()[0]
+
+    #number of N2 reviews
+    cursor.execute("SELECT COUNT(*) FROM ReviewHistory WHERE level='N2'")
+    n2=cursor.fetchone()[0]
+
+    #number of N1 reviews
+    cursor.execute("SELECT COUNT(*) FROM ReviewHistory WHERE level='N1'")
+    n1=cursor.fetchone()[0]
+
+    conn.close()
+
+    total=correct+incorrect
+
+    correct_percentage=round((correct/total)*100, 2)
+    incorrect_percentage=round((incorrect/total)*100, 2)
+
+    return render_template("statistics.html", correct=correct, incorrect=incorrect, total=total, n5=n5, n4=n4, n3=n3, n2=n2, n1=n1, correct_percentage=correct_percentage, incorrect_percentage=incorrect_percentage)
 
 #run flask
 if __name__=="__main__":
--- database.py
+++ database.py
@@ -5,14 +5,14 @@ conn = sqlite3.connect("vocabulary.db")
 cursor = conn.cursor()
 
 #creating the table
-cursor.execute("CREATE TABLE IF NOT EXISTS Vocabulary(id INTEGER PRIMARY KEY, original TEXT, furigana TEXT, english TEXT, level TEXT) ")
+cursor.execute("CREATE TABLE IF NOT EXISTS Vocabulary(id INTEGER PRIMARY KEY, original TEXT, furigana TEXT, english TEXT, level TEXT, correct INTEGER, incorrect INTEGER, position INTEGER) ")
 
 #inserting every row into the database
 with open("jlpt_vocab.csv", encoding="utf-8") as file:
     reader=csv.DictReader(file)
 
     for row in reader:
-        cursor.execute("INSERT INTO Vocabulary(original, furigana, english, level) VALUES(?, ?, ?, ?)", (row["Original"], row["Furigana"], row["English"], row["JLPT Level"]))
+        cursor.execute("INSERT INTO Vocabulary(original, furigana, english, level, correct, incorrect, position) VALUES(?, ?, ?, ?, ?, ?, ?)", (row["Original"], row["Furigana"], row["English"], row["JLPT Level"], 0, 0, 0))
 
 conn.commit()
 conn.close()

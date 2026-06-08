d219336da50e55a3afe9b86fee6b281825b6b1f4
b0larp3ar <148764066+b0larp3ar@users.noreply.github.com>
2026-06-08 21:00:28 +0530

    Added review history tracking

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
 import sqlite3
-import random
 
 app=Flask(__name__)
 
@@ -9,14 +9,45 @@ app=Flask(__name__)
 def home():
     return render_template("home.html")
 
+#review page
 @app.route("/review/<level>")
 def review(level):
     conn=sqlite3.connect("vocabulary.db")
     cursor=conn.cursor()
-    cursor.execute("SELECT original, furigana, english FROM Vocabulary WHERE level=? ORDER BY RANDOM() LIMIT 1", (level,))
+    cursor.execute("SELECT id, original, furigana, english FROM Vocabulary WHERE level=? ORDER BY RANDOM() LIMIT 1", (level,))
     card=cursor.fetchone()
     conn.close()
-    return render_template("review.html", original=card[0], furigana=card[1], english=card[2], level=level)
+    return render_template("review.html", id=card[0], original=card[1], furigana=card[2], english=card[3], level=level)
+
+@app.route("/correct", methods=["POST"])
+def correct():
+    card_id=request.form["card_id"]
+    level=request.form["level"]
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    conn=sqlite3.connect("reviewHistory.db")
+    cursor=conn.cursor()
+    cursor.execute(f"INSERT INTO ReviewHistory(card_id, level, result, timestamp) VALUES(?, ?, ?, ?)", (card_id, level, "Correct", timestamp))
+
+    conn.commit()
+    conn.close()
+
+    return redirect(f"/review/{level}")
+
+@app.route("/incorrect", methods=["POST"])
+def incorrect():
+    card_id=request.form["card_id"]
+    level=request.form["level"]
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    conn=sqlite3.connect("reviewHistory.db")
+    cursor=conn.cursor()
+    cursor.execute(f"INSERT INTO ReviewHistory(card_id, level, result, timestamp) VALUES(?, ?, ?, ?)", (card_id, level, "Incorrect", timestamp))
+
+    conn.commit()
+    conn.close()
+
+    return redirect(f"/review/{level}")
 
 #run flask
 if __name__=="__main__":

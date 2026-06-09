477f71323a4c337c9d8555139b5035d8c6249e02
b0larp3ar <148764066+b0larp3ar@users.noreply.github.com>
2026-06-09 16:13:04 +0530

    SRS, stats

--- app.py
+++ app.py
@@ -10,37 +10,36 @@ def home():
     return render_template("home.html")
 
 #review page
-a=0
+
+a=0 #current queue position
 @app.route("/review/<level>")
 def review(level):
     conn=sqlite3.connect("vocabulary.db")
     cursor=conn.cursor()
 
     global a
-
-    
     card=()
 
     if a==0:
-        cursor.execute("SELECT id from Vocabulary WHERE position=? ORDER BY RANDOM() LIMIT 1", (a,))
+        cursor.execute("SELECT id from Vocabulary WHERE position=? ORDER BY RANDOM() LIMIT 1", (a,)) #if a=0, choose any card whose position=0
         elementid=cursor.fetchone()[0]
-        cursor.execute("UPDATE Vocabulary SET position=? WHERE id=?", (1, elementid))
+        cursor.execute("UPDATE Vocabulary SET position=? WHERE id=?", (1, elementid)) #set its position to 1
         a+=1
 
     if card==():
-        cursor.execute("SELECT id, original, furigana, english, correct, incorrect, position from Vocabulary WHERE position=? ORDER BY RANDOM()", (a,))
+        cursor.execute("SELECT * from Vocabulary WHERE position=? ORDER BY RANDOM()", (a,)) #choose a card whose position=a
         card=cursor.fetchone()
         
         if card is None:
-            cursor.execute("SELECT id, original, furigana, english, correct, incorrect, position FROM Vocabulary WHERE position=0 ORDER BY RANDOM()")
+            cursor.execute("SELECT * FROM Vocabulary WHERE position=0 ORDER BY RANDOM() LIMIT 1") #if no card has position=a, choose any card whose position=0
             card=cursor.fetchone()
-            cursor.execute("UPDATE Vocabulary SET position=? WHERE id=?", (a, card[0]))
+            cursor.execute("UPDATE Vocabulary SET position=? WHERE id=?", (a, card[0])) #set its position to a
+
         a+=1
 
     conn.commit()
     conn.close()
-    if card==():
-        print("No card selection")
+    
     return render_template("review.html", id=card[0], original=card[1], furigana=card[2], english=card[3], level=level, correct=card[4], incorrect=card[5], position=card[6])
 
 #Updates reviewHistory.db when user reviews a card
@@ -52,13 +51,14 @@ def correct():
     correct=int(request.form["correct"])+1
     incorrect=int(request.form["incorrect"])
 
+    #Updates review history
     conn=sqlite3.connect("reviewHistory.db")
     cursor=conn.cursor()
     cursor.execute(f"INSERT INTO ReviewHistory(card_id, level, result, timestamp) VALUES(?, ?, ?, ?)", (card_id, level, "Correct", timestamp))
-
     conn.commit()
     conn.close()
 
+    #Updates queue position of the card reviewed
     conn=sqlite3.connect("vocabulary.db")
     cursor=conn.cursor()
     cursor.execute("UPDATE Vocabulary SET correct=? WHERE id=?", (correct, card_id))
@@ -66,10 +66,10 @@ def correct():
     total=correct+incorrect
     correct_percentage=(correct/total)*100
 
-    position = max(1, round(10 * (1.05 ** (correct_percentage - 50))))
+    position = max(1, round(10 * (1.05 ** (correct_percentage - 50)))) #finds position
     
     global a
-    cursor.execute("UPDATE Vocabulary SET position=? WHERE id=?", (position+a, card_id))
+    cursor.execute("UPDATE Vocabulary SET position=? WHERE id=?", (position+a, card_id)) #updates position of reviewed cardz
 
     conn.commit()
     conn.close()
@@ -84,13 +84,14 @@ def incorrect():
     correct=int(request.form["correct"])
     incorrect=int(request.form["incorrect"])+1
 
+    #Updates review history
     conn=sqlite3.connect("reviewHistory.db")
     cursor=conn.cursor()
     cursor.execute(f"INSERT INTO ReviewHistory(card_id, level, result, timestamp) VALUES(?, ?, ?, ?)", (card_id, level, "Incorrect", timestamp))
-
     conn.commit()
     conn.close()
 
+    #Updates queue position of the card reviewed
     conn=sqlite3.connect("vocabulary.db")
     cursor=conn.cursor()
     cursor.execute("UPDATE Vocabulary SET incorrect=? WHERE id=?", (incorrect, card_id))
